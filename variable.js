f5c2c9fd2ffc3a522435afdf365866b4e51c705a
PUSHKAR SINGH GURJAR <116344643+pushkargurjar1234@users.noreply.github.com>
2024-06-15 16:41:29 +0000

    commit on variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+let marks=10
+const number=19
+var name="pushkar";
+console.table([marks,number,name])
+marks=20
+// number=60
+name="sachin"
+console.log(marks)
+console.log(number)
+console.log(name)
+// var is not used in today life because of block scope and funtion scope.
