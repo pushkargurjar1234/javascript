177195ba6c99678f38a654d1c0f620b16e8d0fee
PUSHKAR SINGH GURJAR <116344643+pushkargurjar1234@users.noreply.github.com>
2024-06-15 17:40:20 +0000

    conversion commit

---
+++ 03_Convesion.js
@@ -0,0 +1,8 @@
+// In this we will convert one type to another.,
+
+let score=50;
+console.log(score)
+
+console.log(typeof score);
+let stringnumber=String(score)
+console.log(typeof stringnumber);
